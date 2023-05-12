3dcd1d6b41cbc8e6db863e8c4ad5d696c8ac4867
Amna Shaikh <shaikhamna19@gmail.com>
2023-05-12 23:34:45 +0500

    Create simple_interest.py

---
+++ simple_interest.py
@@ -0,0 +1,20 @@
+
+
+
+# Python3 program to find simple interest
+# for given principal amount, time and
+# rate of interest.
+#Author (amna-shaikh)
+ 
+ 
+def simple_interest(p,t,r):
+    print('The principal is', p)
+    print('The time period is', t)
+    print('The rate of interest is',r)
+     
+    Simple_interest = (p * t * r)/100
+     
+    print('The Simple Interest is', Simple_interest)
+    return si
+     
+simple_interest(10, 3, 12)
